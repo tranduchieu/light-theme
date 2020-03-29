5873a7c1048606031839737d0ef21dc367f56c44
Tran Duc Hieu <hieu.tranduc@gmail.com>
2020-03-29 17:57:20 +0700

    coding

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-    plugins: process.env.NODE_ENV === 'production' ? [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('cssnano')({
-            preset: 'default'
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+          require('tailwindcss'),
+          require('autoprefixer'),
+          require('cssnano')({
+            preset: 'default',
           }),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './public/**/*.html',
-                './amp/**/*.html'
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-    ] : [
-        require('tailwindcss'),
-        require('autoprefixer')
-    ]
-}
+          require('@fullhuman/postcss-purgecss')({
+            content: ['./public/**/*.html', './amp/**/*.html'],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }),
+        ]
+      : [require('tailwindcss'), require('autoprefixer')],
+};
